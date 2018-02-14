c9813c9448a55736c253567033167578e2cd1cf8
Joshua Westerheide <dev@jdoubleu.de>
2018-02-14 18:16:25 +0100

    Remove unused imports

--- Classes/Command/FluidTemplatePrecompileCommand.php
+++ Classes/Command/FluidTemplatePrecompileCommand.php
@@ -6,8 +6,6 @@ use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Logger\ConsoleLogger;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3Fluid\Fluid\Core\Cache\FluidCacheWarmupResult;
