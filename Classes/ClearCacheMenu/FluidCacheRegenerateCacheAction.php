d9f4389eba06c5cb5ae6abe914799a16e8cb4439
jboesche <j.boesche@neusta.de>
2019-08-14 10:48:45 +0200

    [FEATURE] Add request and response interface to fluid cache regenerate backend action

--- Classes/ClearCacheMenu/FluidCacheRegenerateCacheAction.php
+++ Classes/ClearCacheMenu/FluidCacheRegenerateCacheAction.php
@@ -1,4 +1,5 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace NamelessCoder\CmsFluidPrecompilerModule\ClearCacheMenu;
 
 use TYPO3\CMS\Backend\Routing\UriBuilder;
--- Classes/Controller/FluidPrecompileController.php
+++ Classes/Controller/FluidPrecompileController.php
@@ -1,7 +1,10 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace NamelessCoder\CmsFluidPrecompilerModule\Controller;
 
 use NamelessCoder\CmsFluidPrecompilerModule\Service\FluidPrecompilerService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 
@@ -13,8 +16,10 @@ class FluidPrecompileController
     /**
      * @return void
      */
-    public function precompileAction()
+    public function precompileAction(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         GeneralUtility::makeInstance(ObjectManager::class)->get(FluidPrecompilerService::class)->warmup();
+
+        return $response;
     }
 }
